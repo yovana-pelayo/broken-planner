b7b39dbb1fdc2e170e10e40a7442cd4acf2e548d
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-05-16 15:45:18 -0700

    list of entrries displays to the right of the page as well as the entry count

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
+import { useEntries } from '../../context/PlannerContext';
 import styles from './Header.css';
 
 export default function Header() {
+  const { entries } = useEntries([]);
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
       {/* TODO: Replace hard-coded count with the actual number of entries */}
-      <span>1 Item Left</span>
+      <span>{entries.length}Item Left</span>
     </header>
   );
 }
